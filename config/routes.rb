68c7dbeba32740cd0ed91310442f37e656d5ffb7
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-06-13 17:09:34 -0300

    checking for heroku commit/push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #these names spaces add the paths, aka, 'api/v1'
+  # these names spaces add the paths, aka, 'api/v1'
   namespace :api do
     namespace :v1 do
       #we get musify/login to login controller, spotify auth action
